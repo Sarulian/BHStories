ae0cbf6d637432956772a4c99390672c62c16ad8
Sarulian <tedjamesrobinson@gmail.com>
2017-05-23 15:29:30 -0700

    replaced dummy text with python list storing user input

--- flask_app.py
+++ flask_app.py
@@ -1,13 +1,20 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request, url_for
 
 
 app = Flask(__name__)
 
+words = []
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def active_story():
-    return render_template('active_story.html')
+
+	if request.method == 'GET':
+		return render_template('active_story.html', words=words)
+
+	words.append(request.form['new_word'])
+	return redirect(url_for('active_story'))
 
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=4444)
+    #app.run(debug=True, port=4444)
