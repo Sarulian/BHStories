cbd8cbde2c669cfa8997268411d4b58a935e7fa9
Sarulian <tedjamesrobinson@gmail.com>
2017-05-23 14:18:55 -0700

    now hosting openly

--- app.py
+++ app.py
@@ -10,5 +10,4 @@ def active_story():
 
 
 if __name__ == '__main__':
-    #app.run(debug=True, host='0.0.0.0', port=4444)
-    app.run(debug=True, port=4444)
+    app.run(debug=True, host='0.0.0.0', port=4444)
