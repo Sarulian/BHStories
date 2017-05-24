2fd3323b31b7907cce3ba91e069462a48f52df90
Sarulian <tedjamesrobinson@gmail.com>
2017-05-23 17:56:52 -0700

    adding comments for clarity

--- flask_app.py
+++ flask_app.py
@@ -11,8 +11,10 @@ users = {}
 @app.route('/', methods=['GET', 'POST'])
 def active_story():
 
+	# Just show page if user accesses for first time/refreshes
 	if request.method == 'GET':
 		return render_template('active_story.html', entries=entries)
+	# Otherwise user has inputted a new word
 
 	# Get user IP address and the word they inputted
 	ip_addr = request.environ['REMOTE_ADDR']
@@ -21,7 +23,7 @@ def active_story():
 	# Limit user input to one word
 	word = BHStories.limit_one_word(user_input)
 
-	# If user name is entered store with user IP, otherwise lookup. If no name is found return IP address.
+	# If user name is entered, save with user IP, otherwise lookup. If no name is found return IP address.
 	if request.form['your_name'] != '':
 		name = request.form['your_name']
 		users[ip_addr] = name
@@ -30,11 +32,12 @@ def active_story():
 	else:
 		name = ip_addr
 
+	# Save new word and name to display
 	entries.append([word, name])
 
+	# Refresh page
 	return redirect(url_for('active_story'))
 
 
 if __name__ == '__main__':
-    #app.run(debug=True, host='0.0.0.0', port=4444)
     app.run(debug=True, port=4444)
