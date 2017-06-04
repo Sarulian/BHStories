4c03766bd42295e273275af13db79bcf5d2b2d89
Sarulian <tedjamesrobinson@gmail.com>
2017-06-04 13:03:15 -0700

    completed basic login and lobby with username and id in python

--- flask_app.py
+++ flask_app.py
@@ -1,16 +1,17 @@
 from flask import Flask, redirect, render_template, request, url_for
 from datetime import datetime
-import BHStories
 import json
 
 
 app = Flask(__name__)
 
-# [word1,word2,word3,...]
-entries = []
+# [
+#	[user1.index, 'user1.name'],
+# 	[user2.index, 'user2.name']
+# ]
+users = []
 
-# {IP address:[name, time since last input]}
-users = {}
+i=0
 
 # {
 # 	'prev_image': 'prev_image',
@@ -38,70 +39,10 @@ class User:
 		return self.index
 
 
-
-# Returns only the first word of a series of words
-def limit_one_word(user_input):
-
-	return user_input.split()[0]
-
-
-# Returns the time since the last input was made by this user
-def get_time_limit(ip_addr):
-
-	if ip_addr in users:
-		now = datetime.now()
-		last_input = users[ip_addr][1]
-		return now - last_input
-	else:
-		return 0
-
-
-# If user name is entered, save with user IP, otherwise lookup. If no name is found return IP address.
-def get_name(name_input, ip_addr):
-
-	name = ip_addr
-
-	if name_input != '':
-		name = name_input
-		users[ip_addr][0] = name
-	elif ip_addr in users:
-		name = users[ip_addr][0]
-
-	return name
-
-
-@app.route('/active_story', methods=['GET', 'POST'])
-def active_story():
-
-	# Just show page if user accesses for first time/refreshes
-	if request.method == 'GET':
-		return render_template('active_story.html', entries=entries)
-	# Otherwise user has inputted a new word
-
-	# Get user IP address and the word they inputted
-	ip_addr = request.environ['REMOTE_ADDR']
-	user_input = request.form['new_word']
-
-	# Limit user input to one word
-	word = limit_one_word(user_input)
-
-	# If user name is entered, save with user IP, otherwise lookup. If no name is found return IP address.
-	name = get_name(request.form['your_name'], ip_addr)
-
-	print(get_time_limit(ip_addr))
-
-	# Save new word and name to display
-	entries.append([word, name])
-
-	# Record time of input
-	# users[ip_addr][1] = datetime.now()
-
-	# Refresh page
-	return redirect(url_for('active_story'))
-
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+	global users
+	global i
 
 	if request.method == 'GET':
 
@@ -111,7 +52,25 @@ def login():
 
 		new_user_name = request.form['username']
 
-		
+		new_user = User(new_user_name, i)
+		i+=1
+
+		users.append([new_user.get_index(), new_user.get_name()])
+
+		return redirect(url_for('lobby'))
+
+
+@app.route('/lobby', methods=['GET','POST'])
+def lobby():
+	global users
+
+	if request.method == 'GET':
+
+		return render_template('lobby.html', users=users)
+
+	if request.method == 'POST':
+
+		return redirect(url_for('in_game'))
 
 
 @app.route('/playing', methods=['GET','POST'])
@@ -120,8 +79,8 @@ def in_game():
 
 	if request.method == 'GET':
 
-		print(prev_doodle[1])
-		print(len(prev_doodle[0]))
+		#print(prev_doodle[1])
+		#print(len(prev_doodle[0]))
 
 		return render_template('ingame.html', prevdoodle=prev_doodle)
 
@@ -144,17 +103,8 @@ def in_game():
 @app.route('/', methods=['GET'])
 def start_menu():
 
-	return render_template('start_menu.html')
+	return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
-	#app.run(debug=True, host='0.0.0.0', port=4444)
-	new_user = User("Ted", 0)
-	print("Created new user.")
-	print("Username: %s" %new_user.get_name())
-	print("Place in order: %i" %new_user.get_index())
-	print("Updating user info.")
-	new_user.set_name("Scott")
-	new_user.set_index(1)
-	print("Username: %s" %new_user.get_name())
-	print("Place in order: %i" %new_user.get_index())
+	app.run(debug=True, host='0.0.0.0', port=4444)
